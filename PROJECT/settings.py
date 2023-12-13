3215ffded995ab074329112266f1936b0b44568c
mohamed.sameh <sistar32.m@gmail.com>
2023-12-13 15:51:10 +0200

    debug

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -22,7 +22,7 @@ ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 # Application definition
 
 INSTALLED_APPS = [
-    # 'jazzmin',
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
